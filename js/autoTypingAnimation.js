2c299442fbbfb74c81507ea59231507d1eea3200
Ayush-David <145488546+Ayush-David@users.noreply.github.com>
2024-01-17 14:44:02 +0530

    Initial version

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>AI Development: Offer end-to-end AI development services, including problem identification, data collection, model training, and deployment.Providing custom machine learning solutions tailored to the specific needs of clients across various industries: Providing computer vision services for image and video analysis, object detection, facial recognition, and autonomous systems"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
